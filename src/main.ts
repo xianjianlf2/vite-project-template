e7b79945083c0a58adc41737734a7398beba740b
Mark.Xian <xianjiancong@mywind.com.cn>
2022-07-07 10:22:28 +0800

    🐳 chore(chore):

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { toLine } from './utils/index'
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from './app.vue'
 import router from './router'
 import 'normalize.css'
 import './assets/css/index.css'
